13748fb89c7a4ad7b08900b0ad755da44aec5b96
Zakaria Salogo <zakariasalogo@Zakarias-Air.attlocal.net>
2021-04-14 13:25:01 -0500

    update dependencies

--- src/components/Weather.js
+++ src/components/Weather.js
@@ -4,11 +4,11 @@ import React from "react";
 const Weather = props=>(
 
     <div>    
-    {props.city && props.country &&  <p> Location : {props.city} , {props.country}</p> }
-    {props.temperature && <p>temperature : {props.temperature}</p> }
-    {props.humidity &&  <p> humidity: {props.humidity}</p> }
-    {props.description &&   <p> descriptions : {props.description}</p>}
-    {props.error && <p>{props.error} </p>  }      
+            {props.city && props.country &&  <p> Location : {props.city} , {props.country}</p> }
+            {props.temperature && <p>temperature : {props.temperature}</p> }
+            {props.humidity &&  <p> humidity: {props.humidity}</p> }
+            {props.description &&   <p> descriptions : {props.description}</p>}
+            {props.error && <p>{props.error} </p>  }      
      </div>
 );
 
